33e354aec0addd67ed1148cebe5307d39b9e9385
kurokurotho <deoruchi064@gmail.com>
2023-06-06 18:17:12 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "D:\nft-preview-card-component-main (1)\nft-preview-card-component-mainindex.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
